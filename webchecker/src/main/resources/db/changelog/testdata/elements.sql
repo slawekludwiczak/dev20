53e65a5802ee708f123f33d5d9288370a49904c6
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-08-16 15:52:01 +0200

    dodane dane testowe

---
+++ webchecker/src/main/resources/db/changelog/testdata/elements.sql
@@ -0,0 +1,6 @@
+insert into
+    element(id, url, selector, element_value, fetch_time)
+values
+    ('30D550D1-FA96-4292-A255-B7EC402276B2', 'http://example.com', 'div > p > a', 'More information...', '2023-08-16 12:00:00'),
+    ('1DB457FA-3ACB-4257-A91A-4F7F2B61C035', 'http://example2.com', 'h2', 'Sample heading', '2024-07-10 13:44:55'),
+    ('EA719E3E-FEED-435C-ACBE-59A2282C2E6A', 'http://some-random-website.com', 'p.blue', 'Blue heading', '2024-08-16 11:22:33');
